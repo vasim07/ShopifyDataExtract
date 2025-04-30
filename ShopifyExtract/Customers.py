0817617b3bd502aec08f8483ac82fecb100c949d
Vasim Shaikh <cv.vasim@gmail.com>
2025-04-30 13:36:36 +0530

    initial commit

---
+++ ShopifyExtract/Customers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import logging
+import shopify
+from import_creds import shopify_api_key, shopify_api_password, shopify_store_url
+
+columns = 'id,updated_at,source_name,location_id,line_items,shipping_address,payment_details,discount_allocations,tax_lines'
+
+def extract_order():
+    shopify.Order.find(status="any", fields=columns)
+    pass
+
+pd.read_csv()    
