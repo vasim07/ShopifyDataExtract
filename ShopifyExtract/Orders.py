4d6e7ff61f85a9edfb9257af4804b12c7237848b
Vasim Shaikh <cv.vasim@gmail.com>
2025-04-30 10:45:20 +0000

    initial commit

--- ShopifyExtract/Orders.py
+++ ShopifyExtract/Orders.py
@@ -7,7 +7,7 @@ import pandera
 import typing
 from .metadata import start_date, end_date, shopify_store_url, shopify_api_key, shopify_api_password
 
-columns = 'id,updated_at,source_name,location_id,shipping_address,line_items,discount_allocations,tax_lines'
+columns = 'id,updated_at,source_name,location_id,shipping_address,fulfillments,line_items,discount_allocations,tax_lines'
 
 def extract_orders() -> shopify.collection.PaginatedCollection:
     all_orders = []
@@ -19,23 +19,25 @@ def extract_orders() -> shopify.collection.PaginatedCollection:
     return all_orders
 
 def convert_to_dataframe(anorder) -> pd.DataFrame:
-    line_items = pd.json_normalize(anorder.to_dict(), 
-                      record_path=['line_items'],
-                      meta=['id', 'updated_at', 'source_name', 'location_id'],
-                      record_prefix = 'lt_',
+    fulfilled_items = pd.json_normalize(anorder.to_dict(), 
+                      record_path=['fulfillments', 'line_items'],
+                      meta=['id', 'source_name', 'location_id',['fulfillments', 'updated_at']],
+                      record_prefix = 'sh_',
                       errors='ignore'
                       )
+    fulfilled_items[['id', 'fulfillments.updated_at', 'source_name', 'location_id', 'sh_gift_card', 'sh_id', 
+                'sh_fulfillment_status', 'sh_price', 'sh_sku', 'sh_quantity', 'sh_pre_tax_price']]
     
     discount_items = pd.json_normalize(anorder.to_dict(), 
                       record_path=['line_items', 'discount_allocations'],
                       meta=['id', 'updated_at', 'source_name', 'location_id'],
-                      record_prefix = 'lt_',
+                      record_prefix = 'sh_',
                       errors='ignore'
                       )
     tax_items = pd.json_normalize(anorder.to_dict(), 
                       record_path=['line_items', 'tax_lines'],
                       meta=['id', 'updated_at', 'source_name', 'location_id'],
-                      record_prefix = 'lt_',
+                      record_prefix = 'sh_',
                       errors='ignore'
                       )
     pass
