b4c511cc2b35df00c11ad95cdfa1486d22d41a38
Vasim Shaikh <cv.vasim@gmail.com>
2025-04-30 12:30:04 +0000

    updates

--- ShopifyExtract/Orders.py
+++ ShopifyExtract/Orders.py
@@ -3,33 +3,36 @@ import numpy as np
 import os
 import shopify
 import logging
-import pandera
 import typing
 from .metadata import start_date, end_date, shopify_store_url, shopify_api_key, shopify_api_password
 
-columns = 'id,updated_at,source_name,location_id,shipping_address,fulfillments,line_items,discount_allocations,tax_lines'
+shopify.ShopifyResource.set_site(shopify_store_url)
+shopify.ShopifyResource.set_user(shopify_api_key)  #API_KEY
+shopify.ShopifyResource.set_password(shopify_api_password) #API_TOKEN
 
 def extract_orders() -> shopify.collection.PaginatedCollection:
     all_orders = []
-    orderspage = shopify.Order.find(updated_at_min= start_date, updated_at_max=end_date, status="any", fields=columns, limit=250)
+    #columns = 'id,updated_at,source_name,location_id,shipping_address,fulfillments,line_items,discount_allocations,tax_lines'
+    orderspage = shopify.Order.find(updated_at_min= start_date, updated_at_max=end_date, status="any", limit=250)
     all_orders.extend(orderspage)
     while orderspage.has_next_page():
         orderspage = orderspage.next_page()
         all_orders.extend(orderspage)
     return all_orders
 
+# Store as Broze data
 
-def convert_to_dataframe(anorder) -> pd.DataFrame:
+def process_fulfillments(anorder) -> pd.DataFrame:
     # Shipped
     report_date = pd.to_datetime(start_date).strftime('%Y-%m-%d')
-    fulfilled_items = pd.json_normalize(anorder.to_dict(), 
+    shipped_items = pd.json_normalize(anorder.to_dict(), 
                       record_path=['fulfillments', 'line_items'],
-                      meta=['id', 'source_name', 'location_id',['fulfillments', 'updated_at'],['fulfillments', 'lineitems', 'id']],
+                      meta=['id', 'source_name', 'location_id',['fulfillments', 'updated_at']],
                       record_prefix = 'sh_',
                       errors='ignore'
                       )
-    if len(fulfilled_items) > 0:
-        shipped = fulfilled_items[['id', 'fulfillments.updated_at', 'source_name', 'location_id', 'sh_gift_card', 'sh_id', 
+    if len(shipped_items) > 0:
+        shipped = shipped_items[['id', 'fulfillments.updated_at', 'source_name', 'location_id', 'sh_gift_card', 'sh_id', 
                 'sh_fulfillment_status', 'sh_price', 'sh_sku', 'sh_quantity', 'sh_pre_tax_price']]
     # Discounts
         discount_items = pd.json_normalize(anorder.to_dict(), 
@@ -68,11 +71,12 @@ def convert_to_dataframe(anorder) -> pd.DataFrame:
     else:
         pass
 
+def process_refunds(anorder) -> pd.DataFrame:
+    pass
+    
 a = extract_orders()
-dfs = [convert_to_dataframe(a[i]) for i in range(len(a))]
+dfs = [process_fulfillments(a[i]) for i in range(len(a))]
 df = pd.concat(dfs)
 
-shopify.ShopifyResource.set_site(shopify_store_url)
-shopify.ShopifyResource.set_user(shopify_api_key)  #API_KEY
-shopify.ShopifyResource.set_password(shopify_api_password) #API_TOKEN
+
 #shopify.ShopifyResource.clear_session()
